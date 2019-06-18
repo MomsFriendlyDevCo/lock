e3214319a8e2566d384a8d0cd30a811a033dd98b
Matt Carter <m@ttcarter.com>
2019-06-18 10:13:01 +1000

    Initial release

---
+++ index.js
@@ -0,0 +1,166 @@
+var _ = require('lodash');
+var crypto = require('crypto');
+var debug = require('debug')('lock');
+var marshal = require('@momsfriendlydevco/marshal');
+var mongoose = require('mongoose');
+
+var lock = function(options) {
+	this.settings = {
+		...lock.defaults,
+		...options,
+	};
+
+	this.schema; // Mongo schema when database connection completes
+	this.model; // Mongo model when above completes
+
+
+	/**
+	* Set one or more settings
+	* @param {string|Object} key A single key to set or an object to merge
+	* @returns {Lock} This chainable instance
+	*/
+	this.set = (key, val) => {
+		if (!key) {
+			// Pass
+		} else if (_.isString(key)) {
+			_.set(this.settings, key, val);
+		} else {
+			_.merge(this.settings, key);
+		}
+		return this;
+	};
+
+
+	/**
+	* Verify settings and connect to database
+	* @param {Object} [settings] Optional settings object
+	* @returns {Promise} A promise which will resolve when complete
+	*/
+	this.init = settings => Promise.resolve()
+		.then(()=> this.set(settings))
+		// Sanity checks {{{
+		.then(()=> {
+			['mongodb.uri', 'mongodb.collection']
+				.forEach(key => {
+					if (!_.has(this.settings, key)) throw new Error(`Missing setting: ${key}`);
+				});
+		})
+		// }}}
+		.then(()=> mongoose.set('useFindAndModify', false))
+		.then(()=> mongoose.set('useCreateIndex', true))
+		.then(()=> mongoose.connect(this.settings.mongodb.uri, this.settings.mongodb.options))
+		.then(()=> this.schema = new mongoose.Schema({
+			key: {type: mongoose.Schema.Types.String, index: {unique: true}},
+			expiry: {type: mongoose.Schema.Types.Date},
+			created: {type: mongoose.Schema.Types.Date},
+		}))
+		.then(()=> this.model = mongoose.model(this.settings.mongodb.collection, this.schema));
+
+
+	/**
+	* Request if a lock exists
+	* @param {*} key The key to check, if a non-string this is run via hash() first
+	* @returns {Promise <boolean>} A promise which will resolve with a boolean indicating if the lock exists
+	*/
+	this.exists = key => Promise.resolve()
+		.then(()=> key = this.hash(key))
+		.then(()=> this.model.findOne({key}))
+		.then(doc => {
+			var exists = !!doc;
+			debug('Check key exists', key, '?', exists);
+			return exists;
+		});
+
+
+	/**
+	* Attempt to create a lock, throwing if the lock already exists
+	* @param {*} key The key to lock, hashed if necessary via hash()
+	* @param {Object} fields Additional fields to pass, can contain `{created, expiry}`
+	* @returns {Promise <boolean>} A promise which will resolve with true/false if the lock was created
+	*/
+	this.create = (key, fields) => Promise.resolve()
+		.then(()=> key = this.hash(key))
+		.then(()=> debug('Create lock with key', key))
+		.then(()=> this.model.create({
+			key,
+			created: new Date(),
+			expiry: new Date(Date.now()+this.settings.expiry),
+			...fields,
+		}))
+		.then(()=> {
+			debug('Key created', key);
+			return true;
+		})
+		.catch(e => {
+			debug('Error creating key', key, e);
+			return false;
+		});
+
+
+	/**
+	* Release a lock
+	* @param {*} key The key to release, hashed if neccessary via hash()
+	* @returns {Promise <boolean>} A promise which will resolve when the lock has been removed with a boolean indicating whether the lock existed
+	*/
+	this.release = key => Promise.resolve()
+		.then(()=> key = this.hash(key))
+		.then(()=> this.model.deleteOne({key}))
+		.then(()=> true)
+		.catch(()=> false);
+
+
+	/**
+	* Remove all expired locks from the database
+	* @returns {Promise} A promise which will resolve when the cleaning has completed
+	*/
+	this.clean = ()=> this.model.deleteMany({
+		expiry: {$lt: new Date()},
+	});
+
+
+	/**
+	* Remove ALL locks from the database
+	* This is only really useful when debugging
+	* @returns {Promise} A promise which will resolve when the clearing has completed
+	*/
+	this.clear = ()=> this.model.deleteMany();
+
+
+	/**
+	* Return a string varient of a complex object
+	* If passed a string this returns the same value, for everything else an SHA-256 hash is used instead
+	* @param {*} key The key to hash
+	* @returns {string} A hash of the input key
+	*/
+	this.hash = key =>
+		_.isString(key)
+			? key
+			: crypto.createHash('sha256')
+				.update(marshal.serialize(key, {symetric: true}))
+				.digest('hex')
+
+
+	/**
+	* Close the module and release the database connection
+	* @returns {Promise} A promise which will resolve when the connection has been released
+	*/
+	this.destroy = ()=> this.model
+		? mongoose.connection.close()
+		: Promise.resolve();
+
+
+	return this;
+};
+
+lock.defaults = {
+	expiry: 1000 * 60 * 60, // 1 hour
+	mongodb: {
+		uri: 'mongodb://localhost/mfdc-cache',
+		collection: 'locks',
+		options: {
+			useNewUrlParser: true,
+		},
+	},
+};
+
+module.exports = lock;
---
+++ test/test.js
@@ -0,0 +1,68 @@
+var _ = require('lodash');
+var expect = require('chai').expect;
+var Lock = require('..');
+var mlog = require('mocha-logger');
+
+describe('@momsfriendlydevco/lock', ()=> {
+	var lock;
+	before('set up the module', ()=> lock = new Lock());
+	before('initalize', ()=> lock.init());
+	before('clear existing contents', ()=> lock.clear());
+	after('destroy', ()=> lock.destroy());
+
+	it('hash()', ()=> {
+		expect(lock.hash('hello')).to.be.a('string');
+		expect(lock.hash()).to.be.a('string');
+		expect(lock.hash({one: 'One'})).to.be.a('string');
+		expect(lock.hash(123)).to.be.a('string');
+		expect(lock.hash({one: 1, two: 2})).to.be.equal(lock.hash({two: 2, one: 1}));
+	});
+
+	it('should complete a lock lifecycle', ()=> Promise.resolve()
+		.then(()=> lock.exists('hello'))
+		.then(res => expect(res).to.be.false)
+		.then(()=> lock.create('hello'))
+		.then(res => expect(res).to.be.true)
+		.then(()=> lock.exists('hello'))
+		.then(res => expect(res).to.be.true)
+		.then(()=> lock.release('hello'))
+		.then(res => expect(res).to.be.true)
+		.then(()=> lock.exists('hello'))
+		.then(res => expect(res).to.be.false)
+	);
+
+	it('should prevent collisions', ()=> Promise.resolve()
+		.then(()=> Promise.all([
+			lock.create({foo: 'Foo!'}).catch(()=> false),
+			lock.create({foo: 'Foo!'}).catch(()=> false),
+			lock.create({foo: 'Foo!'}).catch(()=> false),
+		]))
+		.then(res => {
+			expect(res.filter(r => r).length).to.be.equal(1);
+			expect(res.filter(r => !r).length).to.be.equal(2);
+		})
+	);
+
+	it('should randomly create and destroy locks', function() {
+		this.timeout(10 * 1000);
+		var stats = {clashes: 0, created: 0};
+		var created = new Set();
+		var create = ()=> new Promise((resolve, reject) => {
+			var id = 'lock-' + _.random(1, 9);
+			lock.create(id)
+				.then(res => {
+					if (res === false) { // Correct response - detected clash
+						stats.clashes++;
+					} else if (!created.has(id) && res === true) { // Correct response - no clash
+						stats.created++;
+					}
+					resolve();
+				})
+				.catch(reject)
+		});
+
+		return Promise.all(Array.from(new Array(100)).map(i => create()))
+			.then(()=> mlog.log('Created', stats.created, 'locks with', stats.clashes, 'clashes'))
+	});
+
+});
